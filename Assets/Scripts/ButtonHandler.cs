b436b8ce550bf874e1418d1b51df4fbd84001c82
Martin Carapia <martin.carapia@outlook.com>
2025-03-06 13:03:43 -0500

    add: end menu and button press logic

--- Assets/Scripts/ButtonHandler.cs
+++ Assets/Scripts/ButtonHandler.cs
@@ -1,15 +1,21 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 
 public class ButtonHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
     public AudioSource hoverSound;    // Reference to the AudioSource for the hover sound
     public float scaleMultiplier = 1.2f;   // The amount to scale the button by on hover
     private Vector3 originalScale;    // Store the original scale to revert back to
+
     void Start() {
         originalScale = transform.localScale;
     }
-
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            SceneManager.LoadScene("endMenu");
+        }
+    }
     public void OnPointerEnter(PointerEventData eventData) {
         transform.localScale = originalScale * scaleMultiplier;
         if (hoverSound != null) {
@@ -20,6 +26,4 @@ public class ButtonHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
     public void OnPointerExit(PointerEventData eventData) {
         transform.localScale = originalScale;
     }
-
-    
 }
