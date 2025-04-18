f5d3a88e72f2b98b65d1cfe0027bc8a2d2583716
unknown <popecm04@pfw.edu>
2025-04-18 12:51:03 -0400

    push force

--- Assets/Scripts/Joe.cs
+++ Assets/Scripts/Joe.cs
@@ -1,20 +1,16 @@
 using UnityEngine;
 using Obi;
-
-ArrayList<ObiColliderBase> segments = new ArrayList<ObiColliderBase>();
 using System.Collections.Generic;
 using System.Collections;
 
-ArrayList.add(this.elements[0].particle1);
-for (int i = 0; i < this.elements.Count ; i++)
-{
-    Arryalist.add(this.elements[i].particle2);
-}
+
 
 [RequireComponent(typeof(ObiSolver))]
 public class Joe : MonoBehaviour {
 
  	ObiSolver solver;
+	
+	
 
 	void Awake(){
 		solver = GetComponent<ObiSolver>();
@@ -31,6 +27,7 @@ public class Joe : MonoBehaviour {
 	void Solver_OnCollision (object sender, ObiNativeContactList e)
 	{
 		var world = ObiColliderWorld.GetInstance();
+		var segments = new List<ObiColliderBase>();
 
 		// just iterate over all contacts in the current frame:
 		foreach (Oni.Contact contact in e)
@@ -41,10 +38,13 @@ public class Joe : MonoBehaviour {
 				ObiColliderBase col = world.colliderHandles[contact.bodyB].owner;
 				if (col != null)
 				{
-					
+					segments.Add(col);
+
 				}
 			}
 		}
 	}
+
+	
 
 }
